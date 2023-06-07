5f995c5e3afc70f59ba02c6e4587771511b84172
Yoshifumi Kitagawa <93709355+Yoshifumi-Kitagawa-2@users.noreply.github.com>
2023-06-07 14:43:49 +0900

    Update best_scrore_submit_6_dl_with_optuna.py

--- competiton_1/best_scrore_submit_6_dl_with_optuna.py
+++ competiton_1/best_scrore_submit_6_dl_with_optuna.py
@@ -4,6 +4,49 @@
 #昨日提出したDLをoptunaで最適化したい
 #層4つ
 
+#必要なライブラリをインポート
+from google.colab import drive
+drive.mount('/content/drive')
+import os
+
+import random
+
+import numpy as np
+import numpy.random as random
+
+import pandas as pd
+from pandas import DataFrame
+
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import linear_model
+from sklearn.linear_model import LinearRegression
+from sklearn.feature_selection import SelectKBest
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.model_selection import StratifiedKFold, cross_validate
+
+!pip install optuna
+import optuna
+
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+
+from keras.callbacks import EarlyStopping
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 #データ読み込み
 train_data = pd.read_csv("/content/drive/MyDrive/Colab Notebooks/GCI/コンペ1/コンペ/data/train.csv")
 x_test_data = pd.read_csv("/content/drive/MyDrive/Colab Notebooks/GCI/コンペ1/コンペ/data/test.csv")
